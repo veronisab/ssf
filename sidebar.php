6518fae406fc5fbb6250691219b165b4781bb330
Veronica Ulrikkeholm <108199135+veronisab@users.noreply.github.com>
2022-08-04 22:47:11 +0200

    Uploaded website

---
+++ sidebar.php
@@ -0,0 +1,8 @@
+<div class="nav">
+  <a href="program.php">Program</a>
+  <a href="about.php">About</a>
+  <a href="join.php">Join!</a>
+  <a href="http://www.facebook.com">Facebook</a>
+  <a href="upcoming">Upcoming festival</a>
+  <a href="past.php">Past festivals</a>
+</div>
